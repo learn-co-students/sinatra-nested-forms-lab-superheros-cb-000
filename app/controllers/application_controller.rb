f07099c9a5a67de45ae00158d8f19ade2a88b186
cptbrkfst <dyl@resonategroup.ca>
2017-09-12 15:15:39 -0400

    completed lab

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,17 @@
 require 'sinatra/base'
-
-class App < Sinatra::Base
+    class App < Sinatra::Base
 
     set :views, Proc.new { File.join(root, "../views/") }
 
+    get '/' do
+        erb :super_hero
+    end
 
+    post '/teams' do
+        @team = Team.new(params[:team])
+        params[:team][:members].each do |member|
+            Hero.new(member)
+        end
+        erb :team
+    end
 end
