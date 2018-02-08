53cf5bbe987abc43845c6e4feeb5cf75d518fcf1
David Kuruvilla <dapawn@gmail.com>
2018-02-08 15:46:55 +0000

    Done.

---
+++ app/models/hero.rb
@@ -0,0 +1,21 @@
+class Hero
+  attr_reader :name, :power, :bio
+
+  @@heros = []
+
+  def initialize(params)
+    @name = params[:name]
+    @power = params[:power]
+    @bio = params[:bio]
+    @@heros << self
+  end
+
+  def self.all
+    @@heros
+  end
+
+  def self.clear
+    @@heros = []
+  end
+
+end
