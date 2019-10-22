93a5334e60f86131f2f1dcd1ad5c54665166f9c0
Efua Mensah-Brown <emensahb@efbelles-laptop.lippuner.ca>
2019-10-22 16:45:38 -0500

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,5 +4,27 @@ class App < Sinatra::Base
 
     set :views, Proc.new { File.join(root, "../views/") }
 
+  get '/' do
+
+    erb :super_hero
+  end
+
+
+  post '/' do
+    @team = Team.new(name: params[:team][:name], motto: params[:team][:motto])
+
+    params[:team][:member].each do |member|
+      Member.new(member)
+    end
+
+    @members = Member.all
+
+    erb :team
+  end
+
+
+
+
+
 
 end
